52fe75e65b5195dfd3ef8438cbaa24622c715553
dzucconi <mail@damonzucconi.com>
2019-07-07 08:11:13 -0400

    Gulp => Parcel

--- gulp/tasks/stylus.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var stylus = require('gulp-stylus');
-var nib = require('nib');
-
-gulp.task('stylus', function () {
-  gulp.src('./src/stylesheets/*.styl')
-    .pipe(stylus({use: [nib()]}))
-    .pipe(gulp.dest('./build'));
-});
--- gulp/tasks/watch.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-
-gulp.task('watch', ['setWatch', 'browserSync'], function() {
-  gulp.watch('src/stylesheets/**', ['stylus']);
-  gulp.watch('src/html/**', ['copy']);
-  // Note: The browserify task handles js recompiling with watchify
-});
